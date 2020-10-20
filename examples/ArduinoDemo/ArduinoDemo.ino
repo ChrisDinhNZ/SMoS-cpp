4bdaa686ec167306d92502e4878a4f6fbba1f4ca
Chris Dinh <dinh.thanhtoan@gmail.com>
2020-10-21 06:52:23 +1300

    wip

--- examples/ArduinoDemo/ArduinoDemo.ino
+++ examples/ArduinoDemo/ArduinoDemo.ino
@@ -7,66 +7,129 @@
 #include "smos.h"
 #include "Arduino.h"
 
+typedef enum DataArrayIndexInfo_e
+{
+   DATA_ARRAY_INDEX_INFO_RESOURCE,
+   DATA_ARRAY_INDEX_INFO_RESOURCE_INFO,
+   DATA_ARRAY_MAX_SIZE
+};
+
+typedef enum ResourceType_e
+{
+   RESOURCE_TYPE_SWITCH = 0x01,
+   RESOURCE_TYPE_ALARM = 0x02
+};
+
+typedef enum ResourceState_e
+{
+   RESOURCE_STATE_OFF,
+   RESOURCE_STATE_ON
+};
+
+typedef struct Resource
+{
+   ResourceType_e    type;
+   ResourceState_e   state;
+};
+
 static SMoS smosService;
+static SMoSObject smosObject;
 static char *hexString;
-unsigned int secSinceBoot;
-static bool done;
-static uint8_t data[SMOS_MAX_DATA_BYTE_LEN];
+static uint16_t hexStringLength;
+static Resource builtInLedResource;
+
+static uint8_t data[DATA_ARRAY_MAX_SIZE];
+
+static void ResetBuiltInLedResource(void)
+{
+   builtInLedResource.type = RESOURCE_TYPE_SWITCH;
+   builtInLedResource.state = RESOURCE_STATE_OFF;
+   pinMode(LED_BUILTIN, OUTPUT);
+   digitalWrite(LED_BUILTIN, LOW);
+}
+
+static void ResetHexString(void)
+{
+   hexString = "";
+   hexStringLength = 0;
+}
+
+static void SetBuiltInLedState(ResourceState_e state)
+{
+   builtInLedResource.state = state;
+
+   if (state == RESOURCE_STATE_ON)
+   {
+      digitalWrite(LED_BUILTIN, HIGH);
+   }
+   else
+   {
+      digitalWrite(LED_BUILTIN, LOW);
+   }
+}
+
+static ResourceState_e GetBuiltInLedState(void)
+{
+   return builtInLedResource.state;
+}
 
 void setup()
 {
-   done = false;
-
    memset(data, 0, sizeof(data));
-   data[0] = 1;
-   data[1] = 1;
-   hexString = "";
+   memset(&smosObject, 0, sizeof(smosObject));
+   ResetHexString();
+   ResetBuiltInLedResource();
 
-   pinMode(LED_BUILTIN, OUTPUT);
-   digitalWrite(LED_BUILTIN, LOW);
    Serial.begin(9600);
    Serial.println("Begin SMoS Demo");
 }
 
 void loop()
 {
-   secSinceBoot = millis() / 1000;
-
-   /* Do something every 30 secs */
-   if (secSinceBoot % 30 == 0)
+   if (Serial.available())
    {
-      digitalWrite(LED_BUILTIN, HIGH);
+      char c = Serial.read();
 
-      if (!done)
+      /* We have already started processing a hex string so carry on processing it. */
+      if (hexStringLength != 0)
       {
-         switch (smosService.smos_EncodeNonConfirmableResponseMessage(2,
-                                                                      SMOS_CONTENT_TYPE_GENERIC,
-                                                                      0,
-                                                                      SMOS_CODE_CLASS_RESP_SUCCESS,
-                                                                      SMOS_CODE_DETAIL_SUCCESS_CHANGED,
-                                                                      4,
-                                                                      data,
-                                                                      hexString))
+         SMoSResult_e result;
+
+         hexString[hexStringLength] = c;
+         hexStringLength++;
+
+         result = smosService.smos_DecodeHexString(hexString, hexStringLength, &smosObject);
+
+         switch (result)
          {
             case SMOS_RESULT_SUCCESS:
-               Serial.println(hexString);
-               break;
+               sdf
+               return;
 
-            case SMOS_RESULT_ERROR_EXCEED_MAX_DATA_SIZE:
             case SMOS_RESULT_ERROR_NULL_POINTER:
-            case SMOS_RESULT_ERROR_ENCODE_MESSAGE:
-            case SMOS_RESULT_UNKNOWN:
-            default:
-               Serial.println("SMOS_RESULT_ERROR");
-               break;
-         }
+               Serial.println("This shouldn't happen");
+               return;
 
-         done = true;
+            case SMOS_RESULT_ERROR_HEX_STRING_INVALID_STARTCODE:
+               ResetHexString();
+               return;
+
+            case SMOS_RESULT_ERROR_NOT_MIN_LENGTH_HEX_STRING:
+            case SMOS_RESULT_ERROR_HEX_STRING_INCOMPLETE:
+               /* String not long enough, do nothing for now. */
+               return;
+
+            case SMOS_RESULT_ERROR_HEX_STRING_INVALID_STARTCODE:
+            case SMOS_RESULT_ERROR_HEX_STRING_INVALID_CHECKSUM:
+            default:
+               ResetHexString();
+               return;
+         }
       }
-   }
-   else
-   {
-      digitalWrite(LED_BUILTIN, LOW);
-      done = false;
+      else if (smosService.smos_IsStartCode(c))
+      {
+         hexString[hexStringLength] = c;
+         hexStringLength++;
+      }
    }
 }
